8c108c8d7aab342e9eceb3185a4029c0ef210906
betsybaileyy <betsy.bailey80@gmail.com>
2019-02-10 21:51:58 -0800

    handlebars installed, bootstrap activated

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+var exphbs = require('express-handlebars')
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+app.get('/', (req, res) => {
+    res.render('home', { msg: 'Yee-haw!' })
+})
+
+app.listen(3000, () => {
+    console.log('app listening on port 3000 (yee-haw!)')
+})
