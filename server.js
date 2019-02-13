398b008945c1d6dfca0efdb853f9ea48b81795d4
betsybaileyy <betsy.bailey80@gmail.com>
2019-02-12 17:17:55 -0800

    able to create posts and save them to database

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 const express = require('express')
 const app = express()
 var exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+const expressValidator = require('express-validator')
+
+require('./data/reddit-db')
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(expressValidator())
+
+require('./controllers/posts.js')(app)
+
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
@@ -11,4 +22,8 @@ app.get('/', (req, res) => {
 
 app.listen(3000, () => {
     console.log('this thing is on!! port 3000')
+})
+
+app.get('/posts/new', (req, res) => {
+    res.render('posts-new', {} )
 })
